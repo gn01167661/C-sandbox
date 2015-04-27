45c0806f259fc4459645844125338898f9cc4116
may <may@maydeMacBook-Pro-3.local>
2015-04-27 14:58:52 +0800

    Initial Commit

---
+++ C sandbox/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C sandbox
+//
+//  Created by may on 2015/4/27.
+//  Copyright (c) 2015年 C. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
